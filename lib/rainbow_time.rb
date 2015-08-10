a521e9548e83282c479a6824e81750fc540d0861
Rainbow Time <rainboo.time@gmail.com>
2015-08-09 23:36:56 -0500

    read settings cleanly

--- lib/rainbow_time.rb
+++ lib/rainbow_time.rb
@@ -9,40 +9,8 @@ info "Welcome to hellscape!"
 
 require_relative 'rainbow_time/settings.rb'
 RainbowTime.load_settings
-pp RainbowTime.settings
-exit
+debug "Final settings: \n" + RainbowTime.settings.pretty_inspect
 
-config_path = ENV['APPDATA'] + '\rainbow_time\settings.yml'
-debug "pwd: #{Dir.pwd}"
-debug "config file: #{config_path}"
-
-settings = nil
-
-if File.exists?(config_path)
-  settings = YAML.load(File.read(config_path))
-else
-  warn "No config file. Using only default settings."
-  settings = {}
-end
-
-
-settings['logfile'] ||= 'C:\rainbow_time\run.log'
-
-settings['deluge_host'] ||= '127.0.0.1'
-settings['deluge_port'] ||= '58846'
-settings['deluge_user'] ||= 'rainbow_time'
-settings['deluge_pass'] ||= 'rainbowrainbowrainbow'
-settings['deluge_label'] ||= 'rainbow-managed'
-
-settings['tv_dir'] ||= 'TV'
-settings['movies_dir'] ||= 'Movies'
-settings['move_completed'] ||= nil # set to dir to keep done and categorized in separate dir
-
-debug "Settings (file and defaults): "
-debug settings.inspect
-
-
-exit
 begin
   require_relative 'deluge_supervisor.rb'
   supe = DelugeSupervisor.new(settings)
--- lib/rainbow_time/settings.rb
+++ lib/rainbow_time/settings.rb
@@ -1,4 +1,5 @@
-require 'hashie/mash'
+require 'hashie'
+include Hashie
 
 module RainbowTime
   class << self
@@ -13,19 +14,40 @@ module RainbowTime
 
       @settings_file_path = File.join(config_base_dir, 'rainbow_time/settings.yml')
       if File.exists?(settings_file_path)
-        YAML.load(File.read(settings_file_path))
+        Mash.load(settings_file_path)
       else
         warn "No config file #{settings_file_path}. Using only default settings."
-        {}
+        Mash.new
       end
     rescue Exception => e
-      warn "Failed to load config file #{settings_file_path}! Using only default settings."
-      {}
+      warn "Failed to load config file #{settings_file_path}! Because '#{e.inspect}'. Using only default settings."
+      Mash.new
+    end
+
+    def default_settings
+      Mash.new({
+        logfile: 'C:/rainbow_time/run.log',
+        folder: {
+          tv: 'TV',
+          movies: 'Movies',
+          videos: 'Videos',
+          finished: 'Finished',
+          incomplete: 'Incomplete'
+        },
+        deluge: {
+          host: '127.0.0.1',
+          port: '58846',
+          user: 'rainbow_time',
+          pass: 'rainbowrainbowrainbow',
+          managed_label: 'rainbow-managed'
+        },
+        move_completed: false
+      })
     end
 
     def load_settings
-      @settings = load_settings_file
-      @settings = {a: "20"}
+      file_settings = load_settings_file
+      @settings = default_settings.merge(file_settings)
     end
 
   end
